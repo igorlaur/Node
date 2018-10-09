fdb4f86c8684178516351969672cd952de93d9c0
igorlaur <39922652+igorlaur@users.noreply.github.com>
2018-10-09 17:02:06 -0300

    Arquivos Node

--- funcionarios/funcionarios.js
+++ funcionarios/funcionarios.js
@@ -1,24 +1,23 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios') // axios vai pegar o arquivo do servidor para nós trabalharmos aqui no vscode
 
-const chineses = f => f.pais === 'China'
+const chineses = f => f.pais === 'Russia'
 const mulheres = f => f.genero === 'F'
 const menorSalario = (func, funcAtual) => {
-    return fun.salario < funcAtual.salario ? func : funcAtual // entao retorne funcionario porque é menor, caso contrario o atual. Sempre comparando
-}                                                       // trocar funcionario para ser funcionario atual
-
+    return func.salario < funcAtual.salario ? func : funcAtual // Esse salário é menor do que o salário do funcionário atual? Se sim, então troca, bota o atual para ser funcionario com menor salário
+}
 
 axios.get(url).then(response => { // Obtém informação do servidor. Faremos requisição em cima da URL
     const funcionarios = response.data // dentro do atributo 'DATA' tenho a lista de funcionários
     console.log(funcionarios) // Imprimir lista de funcionários
 
-    // mulher china com menor salário?
+    // mulher chinesa com menor salário
     const func = funcionarios
         .filter(chineses)
         .filter(mulheres)
-        .reduce(menorSalario)
+        .filter(menorSalario)
 
-    console.log(func)
 
+    console.log(func.chineses)
 })
 
